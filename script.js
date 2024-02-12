7336ff2193847143891774877a7bedad00838986
dCOMMICS <sanwurld@gmail.com>
2024-02-12 21:02:15 +0300

    intial commit

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ alert('stop smoking weed derrick');
 import * as THREE from 'three'
 
 console.log(THREE)
+const scene = new THREE.Scene()
+
+const myGeometry = new THREE.SphereGeometry(1.5, 32, 32)
+
+
+// scene create
+
+const PlayingArea = new THREE.Scene()
+
+// geometry
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// camera
+
