8eca73766ccc842ba0360639b55efa3bde3f6609
dCOMMICS <sanwurld@gmail.com>
2024-02-13 09:25:20 +0300

    initial commit

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ alert('stop smoking weed derrick');
 
 import * as THREE from 'three'
 
+const canvas = document.querySelector('canvas.webgl')
+console.log(canvas)
+
 console.log(THREE)
 const scene = new THREE.Scene()
 
@@ -17,10 +20,28 @@ const PlayingArea = new THREE.Scene()
 // geometry
 
 const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const material = new THREE.MeshBasicMaterial({color: 0xffff00})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// camera
 
+// sizes
+const sizes = {
+    width:800,
+    height:600
+}
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 4
+scene.add(camera)
+
+// rendering
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+}
+
+)
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
 
